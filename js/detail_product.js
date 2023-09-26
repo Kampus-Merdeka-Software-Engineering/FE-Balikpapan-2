17d585dc8cb0322804436a6ffc14431197fc48c2
khaldarns <khaldankn@gmail.com>
2023-09-26 15:35:19 +0700

    update-qty

--- js/detail_product.js
+++ js/detail_product.js
@@ -1,10 +1,40 @@
-// const inputTotal = document.getElementById("counter");
-// const tambahProduk = document.getElementById("tambahProduk");
-// tambahProduk.addEventListener("click", function () {
-//     let currentValue = parseInt(inputTotal.value);
-//     currentValue += 1;
-//     inputTotal.value = currentValue;
-//     });
+const s = document.getElementById("s"),
+m = document.getElementById("m"),
+l = document.getElementById("l"),
+xl = document.getElementById("xl");
+
+const size= ""
+s.addEventListener("click", ()=>{
+  size="s";
+});
+m.addEventListener("click", ()=>{
+  size="m"
+});
+l.addEventListener("click", ()=>{
+  size="l"
+});
+xl.addEventListener("click", ()=>{
+  size="xl"
+});
+
+console.log("size");
+
+
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+num = document.querySelector(".num");
+
+let a = 1
+plus.addEventListener("click", ()=>{
+  a++;
+  num.innerText = a;
+});
+minus.addEventListener("click", ()=>{
+  if(a>1){
+    a--;
+  }
+  num.innerText = a;
+});
 
 
 function prev(){

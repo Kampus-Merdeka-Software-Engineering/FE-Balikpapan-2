d6660c55f490814114c68345a398bbc731fc8b89
khaldarns <khaldankn@gmail.com>
2023-10-04 23:29:10 +0700

    update9

--- js/components/header.js
+++ js/components/header.js
@@ -35,12 +35,12 @@ class Header extends HTMLElement {
 
         <div class="header-2">
             <nav class="navbar">
-                <a href="./landingpage.html">Home</a>
+                <a href="./index.html">Home</a>
                 <a href="./category_tops.html">Tops</a>
                 <a href="./category_bottoms.html">Bottoms</a>
                 <a href="./category_dresses.html">Dresses</a>
                 <a href="./category_shopall.html">Shop All</a>
-                <a href="./landingpage.html#bestsellers">Best Sellers</a>
+                <a href="./index.html#bestsellers">Best Sellers</a>
                 <a href="./about_us.html">About Us</a>
             </nav>
         </div>
@@ -49,7 +49,7 @@ class Header extends HTMLElement {
     
     <nav class="bottom-navbar">
         <ul>
-            <li><a href="./landingpage.html" class="fas fa-home"></a></li>
+            <li><a href="./index.html" class="fas fa-home"></a></li>
             <li>
                 <a href class="fas fa-align-justify"></a>
                 <ul class="dropdown">
@@ -57,7 +57,7 @@ class Header extends HTMLElement {
                 <li><a href="./category_bottoms.html">Bottoms</a></li>
                 <li><a href="./category_dresses.html">Dresses</a></li>
                 <li><a href="./category_shopall.html">Shop All</a></li>
-                <li><a href="./landingpage.html#bestsellers">Best Sellers</a></li>
+                <li><a href="./index.html#bestsellers">Best Sellers</a></li>
                 <li><a href="./about_us.html">About Us</a></li>
                 </ul>
             </li>
